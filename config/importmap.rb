0cc9136244b4d33221498635e9f153126b303fdb
AbhiEvolution <saini.abhishek7777@gmail.com>
2023-02-03 13:01:13 +0530

    first commit

---
+++ config/importmap.rb
@@ -0,0 +1,7 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application", preload: true
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js"
+pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
